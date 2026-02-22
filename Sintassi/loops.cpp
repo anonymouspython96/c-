0bafcf0c52a7e9799e9da62f12c5be30037d8b80
anonymouspython96 <emilianmail96@gmail.com>
2026-02-22 17:37:37 +0100

    loops mapped

---
+++ Sintassi/loops.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    for ( int i = 1; i <= 20; i++ ) {
+        cout << i;
+    }
+
+    while ( x > 0 ) {
+        x--;
+        cout << x; 
+    }
+
+    do {
+        y++;
+        cout << y;
+    } while ( y < 5 );
+}
